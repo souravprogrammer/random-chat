3f59a21ce20cead97db1ccc8ace6aa25ae8407d2
sourav sharma <souravsh1234567@gmail.com>
2024-02-04 22:26:18 +0530

    package

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 // const path = require("path");
 import path from "path";
-
+import NodePolyfillPlugin from "node-polyfill-webpack-pluginc";
 export default {
   entry: "./app/app.js",
   mode: "production",
+  plugins: [new NodePolyfillPlugin()],
+
   output: {
     path: path.resolve(".", "dist"),
     filename: "server.js",
